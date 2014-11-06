9cd6b5ecca09accbe1fc61518a64fcca0db79018
Dmitry <root@uralbash.ru>
2014-11-06 18:02:59 +0500

    Custom page successful! fixes ITCase/pyramid_sacrud#33

--- pyramid_sacrud_example/includes/gallery/__init__.py
+++ pyramid_sacrud_example/includes/gallery/__init__.py
@@ -9,13 +9,9 @@
 """
 Gallery
 """
-from pyramid_sacrud_gallery.views import gallery_view
 
 
 def includeme(config):
-    config.include('pyramid_sacrud_gallery')
     config.add_jinja2_search_path("templates")
-    config.add_route('pyramid_sacrud_page_of_gallery', '/admin/gallery/{id}')
-    config.add_view(gallery_view,
-                    route_name='pyramid_sacrud_page_of_gallery',
-                    renderer='pyramid_sacrud_gallery/gallery_sacrud_page.jinja2')
+    config.include('pyramid_sacrud_gallery')
+    config.include('.page_of_sacrud')
