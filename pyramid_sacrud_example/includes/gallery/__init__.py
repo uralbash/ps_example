dd23d807fe6a6f69e96a7bc339524491f74b697f
Dmitry <root@uralbash.ru>
2014-11-06 17:46:52 +0500

    play with ITCase/pyramid_sacrud#33

--- pyramid_sacrud_example/includes/gallery/__init__.py
+++ pyramid_sacrud_example/includes/gallery/__init__.py
@@ -9,7 +9,13 @@
 """
 Gallery
 """
+from pyramid_sacrud_gallery.views import gallery_view
 
 
 def includeme(config):
     config.include('pyramid_sacrud_gallery')
+    config.add_jinja2_search_path("templates")
+    config.add_route('pyramid_sacrud_page_of_gallery', '/admin/gallery/{id}')
+    config.add_view(gallery_view,
+                    route_name='pyramid_sacrud_page_of_gallery',
+                    renderer='pyramid_sacrud_gallery/gallery_sacrud_page.jinja2')
