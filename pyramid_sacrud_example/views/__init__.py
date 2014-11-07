941058ce3e6cd9ad49fea9a9326aeb62b161b9a1
Dmitry <root@uralbash.ru>
2014-11-07 19:45:17 +0500

    notfound fix

--- pyramid_sacrud_example/views/__init__.py
+++ pyramid_sacrud_example/views/__init__.py
@@ -17,7 +17,7 @@ def add_global(event):
     event['page_menu'] = get_menu
 
 
-# @notfound_view_config(append_slash=True, renderer='404.jinja2')
-# def notfound(request):
-    # request.response.status = 404
-    # return {}
+@notfound_view_config(append_slash=True, renderer='404.jinja2')
+def notfound(request):
+    request.response.status = 404
+    return {}
